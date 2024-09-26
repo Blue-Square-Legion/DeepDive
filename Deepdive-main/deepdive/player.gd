cd41eb1e2fbce406456e9536cb857f74454a74ca
Jeremyisafellowhuman <61198404+Jeremyisafellowhuman@users.noreply.github.com>
2024-09-26 17:50:26 -0400

    Add files via upload
    Player movement, scene, script. white png as player placeholder.

---
+++ Deepdive-main/deepdive/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var speed := 120.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down") 
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
