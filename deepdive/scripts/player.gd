32b5085f6a37e6104f9e6ab7f4efa8599ad806d2
Kyle <polysplitter@gmail.com>
2024-10-03 15:21:58 -0500

    added sonar part glow and 2 parts

--- deepdive/scripts/player.gd
+++ deepdive/scripts/player.gd
@@ -13,8 +13,25 @@ func _process(delta: float) -> void:
 	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("sonar"):
+		trigger_sonar()
+	
+var active_areas = []
 
+func trigger_sonar():
+	$Sprite2D.visible = not $Sprite2D.visible
+	await get_tree().create_timer(0.2).timeout
+	$Sprite2D.visible = not $Sprite2D.visible
+	for part in active_areas:
+		part.sonar_glow()
 
 func _on_sonar_area_entered(area: Area2D) -> void:
-	if area is Part:
-		print("works!")
+	if area is Area2D:
+		active_areas.push_back(area)
+
+func _on_sonar_area_exited(area: Area2D) -> void:
+	if area is Area2D:
+		var index = active_areas.find(area)
+		if index != -1:
+			active_areas.remove_at(index)
