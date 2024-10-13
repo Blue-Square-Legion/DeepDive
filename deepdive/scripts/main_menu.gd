363961b3f7661ee4242240078f423e307de451b7
Kyle <polysplitter@gmail.com>
2024-10-13 15:22:46 -0500

    setup for machine fixin

--- deepdive/scripts/main_menu.gd
+++ deepdive/scripts/main_menu.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 
 # Handles main menu buttons
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/isometric 2d testing.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_options_button_pressed() -> void:
 	# TODO implement sound options and menu
